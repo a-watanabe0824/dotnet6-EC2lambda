8ee9fc434a28bb17ac3272bf6a0f2f872decbaf9
a-watanabe0824 <122011918+a-watanabe0824@users.noreply.github.com>
2023-03-08 01:18:01 +0900

    Add files via upload

---
+++ LambdaFunction/Function.cs
@@ -0,0 +1,75 @@
+using Amazon.EC2;
+using Amazon.Lambda.Core;
+using C_DotNetCore_Launch_EC2.Model;
+using C_DotNetCore_Launch_EC2.Functions;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace C_DotNetCore_Launch_EC2;
+public class Function
+{
+    public static async Task FunctionHandler(RequestModel model, ILambdaContext context)
+    {
+        var client = new AmazonEC2Client();
+        List<string> Ids = new List<string>();
+
+        var str = model.Group;
+
+        if (model.Action != "Status")
+        {
+            if (str == "Sub")
+            {
+                Ids = new List<string>
+                {
+                   Property.EC2ID_01,
+                   Property.EC2ID_02
+                };
+            }
+            else if (str == "Main")
+            {
+                Ids = new List<string>
+                {
+                  Property.EC2ID_03,
+                  Property.EC2ID_04
+                };
+            }
+            else
+            {
+                await Slack.PostSlackErrorAsync(model.Action, str, "Groupの内容が不正です。");
+                return;
+            }
+        }
+
+        switch (model.Action)
+        {
+            case "Launch":
+                Console.WriteLine(str + ":" + "EC2 Launch");
+                await Launch.LaunchEC2(client, Ids, str);
+                break;
+
+            case "Stop":
+                Console.WriteLine(str + "EC2 Stop");
+                await Stop.StopEC2(client, Ids, str);
+                break;
+
+            case "Status":
+                Console.WriteLine("EC2 tatus");
+                List<string> Ec2Ids = new List<string>
+                {
+                    Property.EC2ID_01,
+                    Property.EC2ID_02,
+                    Property.EC2ID_03,
+                    Property.EC2ID_04
+                };
+
+                await EC2Status.CheckState(client, Ec2Ids);
+
+                break;
+
+            default:
+                await Slack.PostSlackErrorAsync(model.Action, str, "Actionの内容が不正です。");
+                return;
+        }
+    }
+}
+
---
+++ LambdaFunction/Functions/Launch.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.EC2;
+using Amazon.EC2.Model;
+
+namespace C_DotNetCore_Launch_EC2.Functions
+{
+    internal class Launch
+    {
+        /// <summary>
+        /// EC2の起動
+        /// </summary>
+        /// <param name="ec2Client"></param>
+        /// <param name="instanceIds"></param>
+        /// <returns></returns>
+        public static async Task LaunchEC2(IAmazonEC2 ec2Client, List<string> instanceId, string group)
+        {
+            var result = await ec2Client.StartInstancesAsync(new StartInstancesRequest(instanceId));
+            var strlist = new List<string>();
+            foreach (var si in result.StartingInstances)
+            {
+                var str = $"{si.CurrentState.Name}" + ":" + $"{si.InstanceId}";
+                strlist.Add(str);
+            }
+            await Slack.PostToSlackChannel(strlist, "Start", group);
+        }
+    }
+}
