1e91374e4f43687771321f1970af6aceba00aa86
a-watanabe0824 <122011918+a-watanabe0824@users.noreply.github.com>
2023-03-08 01:03:41 +0900

    Add files via upload

---
+++ Functions/T01.cs
@@ -0,0 +1,30 @@
+﻿namespace CLib
+{
+    public class T01
+    {
+        public static string ConvListToString(List<string> iList, string iKugiri = "\r\n")
+        {
+
+            string str1 = String.Empty;
+
+            try
+            {
+                if (iList.Count > 0)
+                {
+                    for (int i = 0; i < iList.Count; i++)
+                        str1 += iList[i] + iKugiri;
+                }
+
+                return str1;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Exception Error");
+                return "Exception Error";
+            }
+        }
+    }
+}
+
---
+++ Model/RequestModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace C_DotNetCore_Launch_EC2.Model
+{
+   public class RequestModel
+    {
+    
+        [JsonProperty("group")]
+        public string Group { get; set; }
+
+        [JsonProperty("action")]
+        public string Action { get; set; }
+
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+    }
+}
+
+
